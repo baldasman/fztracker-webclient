a979e59367c65a010e8dcb12640224c101abc18f
tfuzo <tfuzo@hotmail.com>
2020-10-01 16:40:53 +0100

    LASET23

--- src/feature-modules/home/pages/card-add/card-add.component.ts
+++ src/feature-modules/home/pages/card-add/card-add.component.ts
@@ -5,6 +5,10 @@ import { Subscription } from 'rxjs';
 import { CardService } from '@core-modules/core/services/card.service';
 
 
+
+
+
+
 @Component({
   selector: 'app-card-add',
   templateUrl: './card-add.component.html',
@@ -12,13 +16,28 @@ import { CardService } from '@core-modules/core/services/card.service';
 })
 export class CardAddComponent extends Mixin(Core, Animations, Stores) implements OnInit, OnDestroy {
   
- 
+  @Input() cssClasses = '';
+
+  chartOptions6: any = {};
+  fontFamily = '';
+  colorsGrayGray500 = '';
+  colorsGrayGray200 = '';
+  colorsGrayGray300 = '';
+  colorsThemeBaseDanger = '';
+  colorsThemeBasePrimary = '';
+  colorsThemeLightPrimary = '';
 
   private _docSub: Subscription;
 
   constructor(private layoutConfigService: LayoutConfigService, private cardService: CardService) {
     super();
-
+    this.fontFamily = this.layoutConfigService.getConfig('js.fontFamily');
+    this.colorsGrayGray500 = this.layoutConfigService.getConfig('js.colors.gray.gray500');
+    this.colorsGrayGray200 = this.layoutConfigService.getConfig('js.colors.gray.gray200');
+    this.colorsGrayGray300 = this.layoutConfigService.getConfig('js.colors.gray.gray300');
+    this.colorsThemeBaseDanger = this.layoutConfigService.getConfig('js.colors.theme.base.danger');
+    this.colorsThemeBasePrimary = this.layoutConfigService.getConfig('js.colors.theme.base.primary');
+    this.colorsThemeLightPrimary = this.layoutConfigService.getConfig('js.colors.theme.light.primary');
  
   }
 
@@ -29,11 +48,70 @@ export class CardAddComponent extends Mixin(Core, Animations, Stores) implements
       // save uuid to input
     });
  
-
+    this.chartOptions6 = this.getChartOptions6();
 
   }
 
+  getChartOptions6() {
+    return {
+        series: [
+          {
+            name: "CF-ALF",
+            type: "column",
+            data: [8, 8, 8, 10, 12, 24, 10, ]
+          },
+          {
+            name: "CF-EF",
+            type: "column",
+            data: [0, 0, 0, 0, 0, 0, 0,  ]
+          }
+        ],
+        chart: {
+          height: 200,
+          type: "line"
+        
+         
+        },
+
+        toolbar: {
+          show: false
+        },
+
+        stroke: {
+          width: [0, 4]
+        },
+        title: {
+          text: "Corpo de Fuzileiros"
+        },
+        dataLabels: {
+          enabled: false,
+          enabledOnSeries: []
+        },
+        labels: [
+          "01 Nov 2020",
+          "02 Nov 2020",
+          "03 Nov 2020",
+          "04 Nov 2020",
+          "05 Nov 2020",
+          "06 Nov 2020",
+          "07 Nov 2020",
+          
+      
   
+        ],
+        xaxis: {
+          type: "datetime"
+        },
+        yaxis: [
+          {
+            title: {
+              text: "Horas na Unidade"
+            }
+          },
+       
+        ]
+      };
+    };
 
 
 }
