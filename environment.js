1623013bb7f76f39ba25159465a8169874978f75
tfuzo <68619966+tfuzo@users.noreply.github.com>
2020-10-15 09:13:10 +0100

    test

---
+++ environment.js
@@ -0,0 +1,9 @@
+window.__env = {
+    DOMAIN: 'fztracker.pt',
+    API_URL: 'http://localhost:8000',
+    LOG_LEVEL: 'TRACE',
+  
+    WS_PROTOCOL: 'http',
+    WS_HOSTNAME: 'localhost',
+    WS_PORT: 8081
+  };
