6219fc0357675b6cbc2ad5bf47ccdd121ddd2d1a
tfuzo <68619966+tfuzo@users.noreply.github.com>
2020-11-19 14:12:41 +0000

    Delete environment.js

--- environment.js
+++
@@ -1,9 +0,0 @@
-window.__env = {
-    DOMAIN: 'fztracker.pt',
-    API_URL: 'http://localhost:8000',
-    LOG_LEVEL: 'TRACE',
-  
-    WS_PROTOCOL: 'http',
-    WS_HOSTNAME: 'localhost',
-    WS_PORT: 8081
-  };
